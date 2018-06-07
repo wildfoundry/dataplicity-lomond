4d61e75ee8554af1dbd53718ea4fae0eb2bc3da9
Oliver Ebert <oe@outputenable.net>
2018-06-07 22:44:26 +0200

    Make monotonic a mandatory requirement and adjust import

--- lomond/session.py
+++ lomond/session.py
@@ -16,10 +16,7 @@ import threading
 
 from six.moves.urllib.parse import urlparse
 
-try:
-    from monotonic import monotonic as get_time
-except ImportError:
-    from time import time as get_time
+from monotonic import monotonic as monotonic_time
 
 from .frame import Frame
 from . import errors
@@ -64,7 +61,7 @@ class WebsocketSession(object):
         return (
             0.0
             if self._start_time is None else
-            get_time() - self._start_time
+            monotonic_time() - self._start_time
         )
 
     def close(self):
@@ -325,7 +322,7 @@ class WebsocketSession(object):
         """Called when a ready event is received."""
         self._last_pong = 0.0
         self._next_ping = 0.0
-        self._start_time = get_time()
+        self._start_time = monotonic_time()
 
     def _on_event(self, event, auto_pong=True):
         """Handle logic in response to an event."""
